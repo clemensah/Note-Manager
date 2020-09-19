5357643d1444246a694f25d231d17af50397ee49
clemensah <65801831+clemensah@users.noreply.github.com>
2020-09-19 03:38:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+// Lecture: Project - Coding Task 3
+var ul = document.querySelector('ul');
+
+document.getElementById('add-btn').addEventListener('click',function(e){
+    e.preventDefault();
+    var addInput = document.getElementById('add-input');
+    
+    if(addInput.value !== ''){
+    var li = document.createElement('li'),
+        firstPar = document.createElement('p'),
+        secondPar = document.createElement('p'),
+        firstIcon = document.createElement('i'),
+        secondIcon = document.createElement('i'),
+        input = document.createElement('input');
+        
+    firstIcon.className = "fa fa-pencil-square-o";
+    secondIcon.className = "fa fa-times";
+    input.className = "edit-note";
+    input.setAttribute('type', 'text');
+    
+    firstPar.textContent = addInput.value;
+    secondPar.appendChild(firstIcon);
+    secondPar.appendChild(secondIcon);
+    li.appendChild(firstPar);
+    li.appendChild(secondPar);
+    li.appendChild(input);
+    ul.appendChild(li);
+    addInput.value = '';
+    }
+    //console.log(li);
+});
+
+ul.addEventListener('click', function(e) {
+    if(e.target.classList[1] === "fa fa-pencil-square-o"){
+        var parentPar = e.target.paretNode;
+        parentPar.style.display = 'none';
+        
+        var note = parentPar.previousElementSibling;
+        var input = parentPar.nextElementSibling;
+        console.log(note, input);
+        input.style.display = 'block';
+        input.value = note.textContent;
+        
+        input.addEventListener('keypress', function(e) {
+            if(e.keyCode === 13){
+                if(input.value !== ''){
+                console.log('Youu pressed Enter key');
+                note.textContent = input.value;
+                parentPar.style.display = 'none'
+            }else{
+                var li = input.parentNode;
+                li.parentNode.removeChild;
+                }
+            }
+        });
+    }else if(e.target.classList[1] === "fa-times"){
+        var list = e.target.parentNode.parentNode;
+        list.parentNode.removeChild(list);
+    }
+    console.log(e.target.className[1]);
+});
+
+var hideItem = document.getElementById('hide');
+hideItem.addEventListener('click', function(){
+    var label = document.querySelector('label');
+    
+    if(hideItem.checked){
+        ul.style.display = 'none';
+        label.textContent = 'Unhide notes';
+    }else{
+        label.textContent = 'Hide nptes';
+        ul.style.display = 'block';
+    }
+});
+
+var searchInput = document.getElementById('#search-note input');
+
+searchInput.addEventListener('keyup', function(){
+    var searchChar = e.target.value.toUpperCase();
+    
+    var notes = ul.getElementsByTagName('li');
+    
+    Array.from(notes).forEach(function(note){
+        var parText = note.firstElementChild.textContent;
+        
+        if(parText.toUpperCase().indexOf(searchChar) !== -1){
+            note.style.display = 'block';
+            
+        }else{
+            note.style.display = 'none';
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
